9e36cb1264b89da0bc655072bf0000dbedc7a1f7
Bryann Henrique <bryann.enrique@gmail.com>
2023-04-06 01:12:04 -0300

    feat: add account usecase implementation

--- src/presentation/controllers/sugnup.ts
+++ src/presentation/controllers/sugnup.ts
@@ -1,3 +1,4 @@
+import { AddAccount } from '../../domain/usecases/add-account';
 import { InvalidParamError, MissinParamError } from '../errors';
 import { badRequest, serverError } from '../helpers/http-helpers';
 import {
@@ -9,9 +10,11 @@ import {
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly addAccount: AddAccount;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -21,6 +24,7 @@ export class SignUpController implements Controller {
         'password',
         'passwordConfirmation',
       ];
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
 
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
@@ -28,11 +32,21 @@ export class SignUpController implements Controller {
         }
       }
 
-      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+
+      const isValidEmail = this.emailValidator.isValid(email);
 
       if (!isValidEmail) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password,
+      });
 
       return {
         statusCode: 201,
